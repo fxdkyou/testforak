e1b0e20fc15b3158f53aa08584e584d33e758390
fxdkyou <43986611+fxdkyou@users.noreply.github.com>
2022-03-01 11:18:40 +0800

    Create test.sql

---
+++ test.sql
@@ -0,0 +1,2 @@
+INSERT INTO `tbsystemparam` VALUES ('161', '阿里短信发送', 'alisms_accessKeyId', 'LTAI5tNuZPFo3wkz6jKscaZX', '1', null, null);
+INSERT INTO `tbsystemparam` VALUES ('162', '阿里短信发送', 'alisms_accessKeySecret', '2Wf8dtrbogpJlPdxcilJppxkxOHjvs', '1', null, null);
